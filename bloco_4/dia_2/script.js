d7130e7528784ba409fe2e3f57c0f8ad6005ebcc
Silvio Fabian <silviofabian334@gmail.com>
2021-05-20 19:41:32 -0300

    Exercicios bloco4 dia2

---
+++ bloco_4/dia_2/script.js
@@ -0,0 +1,44 @@
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  
+
+tasksList.unshift('Tomar banho');
+
+console.log(tasksList);
+
+//Exercício 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+*/
+//Exercício 2
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
+*/
+//Exercício 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
+
+//Exercício 4
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
+
+//Exercicio 5
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (name1 of names) {
+  console.log(names);
+}
+
+console.log(names)
