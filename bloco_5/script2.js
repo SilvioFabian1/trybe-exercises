a5e5b88a961bf9583003112f44a900674e5c720b
Silvio Fabian <silviofabian334@gmail.com>
2021-05-27 10:59:16 -0300

    Exercicios bloco 5 dia 1

---
+++ bloco_5/script2.js
@@ -0,0 +1,13 @@
+document.querySelector("body").style.backgroundColor = "gray";
+
+document.getElementById("header-container").style.backgroundColor = "green";
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "red";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "blue";
+
+document.querySelectorAll("h3")[0].style.backgroundColor = "crimson";
+document.querySelectorAll("h3")[1].style.backgroundColor = "crimson";
+document.querySelectorAll("h3")[3].style.backgroundColor = "pink";
+document.querySelectorAll("h3")[2].style.backgroundColor = "pink";
+
+document.getElementById("footer-container").style.backgroundColor = "black";
