958264565e25578f9c79db1c79e90463567b0385
Silvio Fabian <silviofabian334@gmail.com>
2022-04-05 16:49:56 -0300

    Primeiro commit em sei la quantos meses

---
+++ Módulo 1 - Fundamentos do Desenvolvimento WEB/bloco_7/dia_1/voltando.js
@@ -0,0 +1,14 @@
+// Função padrão
+function soma (num1, num2) {
+  return console.log(num1 + num2);
+}
+
+// Arrow Function
+const soma = (num1, num2) => {
+  return console.log(num1 + num2);
+};
+
+// Com um código que vai apenas uma linha
+const soma = (num1, num2) => console.log(num1 + num2);
+
+soma(1, 2);
